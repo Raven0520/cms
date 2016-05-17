232b2fb59b06455eabc5591cece5e8f5da58b475
Raven <raven.259695@gmail.com>
2016-05-17 17:47:27 +0800

    完成推荐位内容管理  添加功能编写并测试

--- Application/Admin/Controller/PositioncontentController.class.php
+++ Application/Admin/Controller/PositioncontentController.class.php
@@ -8,6 +8,7 @@
 
 namespace Admin\Controller;
 use Think\Controller;
+use Think\Exception;
 
 class PositioncontentController extends CommonController
 {
@@ -27,6 +28,47 @@ class PositioncontentController extends CommonController
         $this->assign('contents',$contents);
         $this->assign('position_id',$data['position_id']);
 
+        $this->display();
+    }
+
+    public function add(){
+        //判断传入数值是否正确
+        if ($_POST){
+            if (!$_POST['position_id'] || !isset($_POST['position_id'])){
+                return show(0,'推荐位ID不能为空');
+            }
+            if (!$_POST['title'] || !isset($_POST['title'])){
+                return show(0,'推荐位标题不能为空');
+            }
+            if (!$_POST['url'] && !$_POST['news_id']){
+                return show(0,'url和news_id不能同时为空');
+            }
+            if (!$_POST['thumb'] || !isset($_POST['thumb'])){
+                if ($_POST['news_id']){
+                    $res = D("News")->find($_POST['news_id']);
+                    if ($res && is_array($res)){
+                        $_POST['thumb'] = $res['thumb'];
+                    }
+                }else{
+                    return show(0,'图片不能为空');
+                }
+            }
+            //插入数据库
+            try{
+                $id = D("PositionContent")->insert($_POST);
+                if ($id){
+                    return show(1,'新增成功');
+                }
+                return show(0,'新增失败');
+            }catch (Exception $e){
+                return show(0,$e->getMessage());
+            }
+
+        }else {
+            $positions = D("Position")->getNormalPositions();
+
+            $this->assign('positions', $positions);
+        }
         $this->display();
     }
 }
